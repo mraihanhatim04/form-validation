13f5dcdef4e68eb7917fb35ee9b46d9680d102a3
mraihanhatim <raihan04042001@gmail.com>
2024-10-26 13:50:09 +0700

    edit ui home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
+          className="bg-white py-4 px-10 rounded-xl"
           src="/next.svg"
           alt="Next.js logo"
           width={180}
@@ -15,15 +15,24 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <p className="mb-2 text-xl font-serif">Form-Validation with zod</p>
+          <p className=" text-2xl font-serif flex flex-col">
+            Form-Validation
+            <span className="text-lg mt-4">zod</span>
+            <span className="text-lg">react-hook-form</span>
+            <span className="text-lg mb-2">hookform-resolvers</span>
+          </p>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Link href={"/login"}>
-            <Button>Login Form</Button>
+            <Button className="bg-slate-200 text-black hover:bg-slate-300">
+              Login Form
+            </Button>
           </Link>
           <Link href={"/register"}>
-            <Button>Register Form</Button>
+            <Button className="bg-slate-200 text-black hover:bg-slate-300">
+              Register Form
+            </Button>
           </Link>
         </div>
       </main>
