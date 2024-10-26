2eaed00f578b0f01f6d02c93fd5270ac6ef22290
mraihanhatim <raihan04042001@gmail.com>
2024-10-26 13:10:50 +0700

    create register formschema validation

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { useState } from "react"; // Import useState
+import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -26,57 +26,82 @@ import {
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 // Define the schema for validation using zod
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-  password: z
-    .string()
-    .min(8, { message: "Password must be at least 8 characters." })
-    .refine((value) => /[A-Z]/.test(value), {
-      message: "Password must contain at least one uppercase letter.",
-    })
-    .refine((value) => /[0-9]/.test(value), {
-      message: "Password must contain at least one number.",
-    })
-    .refine((value) => /[!@#$%^&*(),.?":{}|<>]/.test(value), {
-      message: "Password must contain at least one special character.",
+const formSchema = z
+  .object({
+    email: z.string().email({ message: "Invalid email address." }),
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
     }),
-});
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters." })
+      .refine((value) => /[A-Z]/.test(value), {
+        message: "Password must contain at least one uppercase letter.",
+      })
+      .refine((value) => /[0-9]/.test(value), {
+        message: "Password must contain at least one number.",
+      })
+      .refine((value) => /[!@#$%^&*(),.?":{}|<>]/.test(value), {
+        message: "Password must contain at least one special character.",
+      }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match.",
+    path: ["confirmPassword"],
+  });
 
-export default function Login() {
+export default function Register() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      email: "",
       username: "",
       password: "",
+      confirmPassword: "",
     },
   });
 
   // State to control password visibility
   const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    alert(`Username: ${values.username}`);
-    alert(`Password: ${values.password}`);
+  function handleRegister(values: z.infer<typeof formSchema>) {
     console.log(values);
   }
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Card className="w-[400px]">
+    <div className="flex flex-col items-center justify-center h-screen p-12">
+      <Card className="w-[450px]">
         <CardHeader>
-          <CardTitle className="text-3xl">Login</CardTitle>
+          <CardTitle className="text-3xl">Register</CardTitle>
           <CardDescription className="font-semibold text-md text-slate-700">
             Please enter your details ✨
           </CardDescription>
         </CardHeader>
         <Form {...form}>
           <form
-            onSubmit={form.handleSubmit(onSubmit)}
+            onSubmit={form.handleSubmit(handleRegister)}
             className="space-y-4 w-full"
           >
             <CardContent className="space-y-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your Email"
+                        type="email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
               <FormField
                 control={form.control}
                 name="username"
@@ -115,6 +140,37 @@ export default function Login() {
                         onClick={() => setShowPassword((prev) => !prev)} // Toggle visibility
                       >
                         {showPassword ? (
+                          <FaEye className="h-5 w-5" />
+                        ) : (
+                          <FaEyeSlash className="h-5 w-5" />
+                        )}
+                      </button>
+                    </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <div className="relative">
+                      <FormControl>
+                        <Input
+                          placeholder="********"
+                          type={showConfirmPassword ? "text" : "password"} // Toggle type
+                          {...field}
+                        />
+                      </FormControl>
+                      <button
+                        type="button"
+                        className="absolute inset-y-0 right-0 flex items-center pr-3"
+                        onClick={() => setShowConfirmPassword((prev) => !prev)} // Toggle visibility
+                      >
+                        {showConfirmPassword ? (
                           <FaEye className="h-5 w-5" />
                         ) : (
                           <FaEyeSlash className="h-5 w-5" />
